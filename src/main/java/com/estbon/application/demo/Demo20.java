283b034c6e80c7081e22463858419f7ac815b713
Shuaishuai Liu <liushuaishuai@juran.com.cn>
2019-03-27 19:38:57 +0800

    update

--- src/main/java/com/estbon/application/demo/Demo20.java
+++ src/main/java/com/estbon/application/demo/Demo20.java
@@ -1,10 +1,5 @@
 package com.estbon.application.demo;
 
-import java.util.AbstractMap;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * @author liushuaishuai
@@ -12,66 +7,11 @@ import java.util.Set;
  * @date 2019/3/2 17:59
  * @description
  */
-public class Demo20 implements Map {
+public class Demo20 {
+
+    public static void main(String[] args) {
 
 
-    @Override
-    public int size() {
-        return 0;
-    }
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        return false;
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        return false;
-    }
-
-    @Override
-    public Object get(Object key) {
-        return null;
-    }
-
-    @Override
-    public Object put(Object key, Object value) {
-        return null;
-    }
-
-    @Override
-    public Object remove(Object key) {
-        return null;
-    }
-
-    @Override
-    public void putAll(Map m) {
-
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public Set keySet() {
-        return null;
-    }
-
-    @Override
-    public Collection values() {
-        return null;
-    }
-
-    @Override
-    public Set<Entry> entrySet() {
-        return null;
     }
 }
