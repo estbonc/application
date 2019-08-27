addb71137fe102fc1ea559120d1f35493c41d6ca
Shuaishuai Liu <liushuaishuai@gaodun.com>
2019-08-27 17:59:22 +0800

    update

--- src/main/java/com/estbon/application/demo/thread/ScheduledService.java
+++ src/main/java/com/estbon/application/demo/thread/ScheduledService.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 /**
  * @author liushuaishuai
  * @date 2019/8/27 15:49
@@ -11,19 +13,16 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class ScheduledService {
-    @Scheduled(cron = "0/5 * * * * *")
+
+    @Scheduled(cron = "0 59 17 * * ?")
     public void scheduled() {
         log.info("=====>>>>>使用cron  {}", System.currentTimeMillis());
+        log.info("当前时间：{}", new Date());
     }
 
-    @Scheduled(fixedRate = 5000)
-    public void scheduled1() {
-        log.info("=====>>>>>使用fixedRate{}", System.currentTimeMillis());
-    }
-
-    @Scheduled(fixedDelay = 5000)
+    @Scheduled(cron = "0/5 * * * * *")
     public void scheduled2() {
-        log.info("=====>>>>>fixedDelay{}", System.currentTimeMillis());
+        log.info("当前时间:{}", new Date());
     }
 
 }
