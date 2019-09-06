c48fa84b61be547e46f0f8d4f01d55e68a0eee39
Shuaishuai Liu <liushuaishuai@gaodun.com>
2019-09-06 09:27:39 +0800

    update

---
+++ src/main/java/com/estbon/application/demo/date/Demo2.java
@@ -0,0 +1,16 @@
+package com.estbon.application.demo.date;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author liushuaishuai
+ * @date 2019/9/5 17:11
+ */
+public class Demo2 {
+
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
+    }
+
+
+}
