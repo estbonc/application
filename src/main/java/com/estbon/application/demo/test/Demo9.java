6e9d6d6635ad468e1dbd9bc46e3410f6f915089d
Shuaishuai Liu <liushuaishuai@gaodun.com>
2020-09-10 13:26:05 +0800

    update

--- src/main/java/com/estbon/application/demo/test/Demo9.java
+++ src/main/java/com/estbon/application/demo/test/Demo9.java
@@ -18,15 +18,30 @@ public class Demo9 {
         a.add(new Apple(2, "yellow"));
         a.add(new Apple(3, "yellow"));
 
-        List<Apple> b = Lists.newArrayList();
-        b.add(new Apple(1, "yellow"));
-        b.add(new Apple(2, "yellow"));
-        a.removeAll(b);
-        a.forEach(System.out::println);
+//        List<Apple> b = Lists.newArrayList();
+//        b.add(new Apple(1, "yellow"));
+//        b.add(new Apple(2, "yellow"));
+//        a.removeAll(b);
+//        a.forEach(System.out::println);
+//
+//
+//        double ceil = Math.ulp(3.01d);
+//        System.out.println(ceil);
+
+        Apple[] users = new Apple[]{
+                new Apple(1, "admin"),
+                new Apple(2, "maco"),
+                new Apple(3, "kitty")
+        };
+
+        Object[] objects = users;
+        System.out.println(objects.getClass());     // class [Ltest.User;
+        objects = new Object[users.length];
+        System.arraycopy(users, 0, objects, 0, Math.min(users.length, objects.length));
+        System.out.println(objects.getClass());     // class [Ljava.lang.Object;
+        System.out.println(objects[0] == users[0]); // true
 
 
-        double ceil = Math.ulp(3.01d);
-        System.out.println(ceil);
 
     }
 }
