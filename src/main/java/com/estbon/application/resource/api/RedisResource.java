501f5ebcdb16b97b1ab754dd88c600260dd61a54
lss <liushuaishuai@gaodun.com>
2021-02-20 10:41:46 +0800

    测试redis

--- src/main/java/com/estbon/application/resource/api/RedisResource.java
+++ src/main/java/com/estbon/application/resource/api/RedisResource.java
@@ -1,23 +1,40 @@
 package com.estbon.application.resourse.api;
 
 import com.estbon.application.redis.queue.RedisMessage;
+import com.xiaoleilu.hutool.geo.Location;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Point;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author liushuaishuai
  * @date 2019/11/29 15:37
  */
+@Api(tags = "redis 测试")
 @RestController
 @RequestMapping("/redis")
 public class RedisResource {
 
-
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @GetMapping("/up")
+    @ApiOperation("test")
+    public void up() {
+        Location location = new Location(1, 2);
+        Map<Object, Point> memberCoordinateMap = new HashMap<>();
+        memberCoordinateMap.put("shanghai", new Point(1, 2));
+        Long add = redisTemplate.opsForGeo().add("123", memberCoordinateMap);
+    }
+
 
     @GetMapping()
     public void put() {
