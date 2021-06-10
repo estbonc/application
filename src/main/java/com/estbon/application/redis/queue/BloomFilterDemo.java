21cd107f59e84b70fe7315aee30ddcdfb0479aa9
Shuiashuai Liu <924829576@qq.com>
2021-06-10 23:28:47 +0800

    update

---
+++ src/main/java/com/estbon/application/redis/queue/BloomFilterDemo.java
@@ -0,0 +1,31 @@
+package com.estbon.application.redis.queue;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+/**
+ * @author liushuaishuai
+ * @date 2021/6/8 23:46
+ */
+public class BloomFilterDemo {
+
+    public static void main(String[] args) {
+        int total = 1000000; // 总数量
+        BloomFilter<CharSequence> bf =
+                BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8), total,0.0002);
+        // 初始化 1000000 条数据到过滤器中
+        for (int i = 0; i < total; i++) {
+            bf.put("" + i);
+        }
+        // 判断值是否存在过滤器中
+        int count = 0;
+        for (int i = 0; i < total + 10000; i++) {
+            if (bf.mightContain("" + i)) {
+                count++;
+            }
+        }
+        System.out.println("已匹配数量 " + count);
+    }
+
+}
